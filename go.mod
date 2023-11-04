cac750f8382bcd4412545b5a5423a363bffe8fe4
mirs <mircea.buciuman93@gmail.com>
2023-11-04 15:11:00 +0200

    extended puppy package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/buciumanmirs/puppy
 
 go 1.21.3
+
+require github.com/buciumanmirs/dog v0.0.0-20231104130500-760a6c694115
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/buciumanmirs/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
